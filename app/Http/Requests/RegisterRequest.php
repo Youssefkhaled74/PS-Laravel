f5b1d279f97e9a21161c0e304d78b043ae142332
youssefkhaled_74 <iyoussefbusinesstowork@gmail.com>
2026-01-14 06:56:02 -0800

    feat: Update accept_terms validation to allow nullable boolean values

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -19,7 +19,7 @@ class RegisterRequest extends FormRequest
             'phone' => ['required','string','max:30'],
             'email' => ['nullable','email','max:191'],
             'password' => ['required','string','min:8','confirmed'],
-            'accept_terms' => ['required','accepted'],
+            'accept_terms' => ['nullable','boolean'],
         ];
     }
 
